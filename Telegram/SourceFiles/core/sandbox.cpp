4d6330327f878117ffe0d7324066a86b4076bb59
ZavaruKitsu <alexeyzavar@gmail.com>
2023-07-10 21:06:49 +0000

    feat: rewrite database part

--- Telegram/SourceFiles/core/sandbox.cpp
+++ Telegram/SourceFiles/core/sandbox.cpp
@@ -34,6 +34,8 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include <QtGui/QScreen>
 #include <QtGui/qpa/qplatformscreen.h>
 
+#include "ayu/database/ayu_database.h"
+
 namespace Core {
 namespace {
 
@@ -118,6 +120,8 @@ int Sandbox::start() {
 			}
 		}
 	}
+
+	AyuDatabase::initialize();
 
 #if defined Q_OS_LINUX && QT_VERSION >= QT_VERSION_CHECK(6, 2, 0)
 	_localServer.setSocketOptions(QLocalServer::AbstractNamespaceOption);
