9d8a18d54459efe0a43b462f47761745f4739937
ZavaruKitsu <alexeyzavar@gmail.com>
2023-08-01 15:47:39 +0000

    feat: add hide "All chats" folder (thx 64Gram)

--- Telegram/SourceFiles/data/data_chat_filters.cpp
+++ Telegram/SourceFiles/data/data_chat_filters.cpp
@@ -25,6 +25,10 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "main/main_app_config.h"
 #include "apiwrap.h"
 
+// AyuGram includes
+#include "ayu/ayu_settings.h"
+
+
 namespace Data {
 namespace {
 
@@ -483,12 +487,17 @@ void ChatFilters::requestToggleTags(bool value, Fn<void()> fail) {
 }
 
 void ChatFilters::received(const QVector<MTPDialogFilter> &list) {
+	// AyuGram hideAllChatsFolder
+	const auto settings = &AyuSettings::getInstance();
+
 	auto position = 0;
 	auto changed = false;
 	for (const auto &filter : list) {
 		auto parsed = ChatFilter::FromTL(filter, _owner);
-		const auto b = begin(_list) + position;
-		const auto e = end(_list);
+		if (settings->hideAllChatsFolder && parsed.id() == 0 && list.size() > 1) {
+			continue;
+		}
+		const auto b = begin(_list) + position, e = end(_list);
 		const auto i = ranges::find(b, e, parsed.id(), &ChatFilter::id);
 		if (i == e) {
 			applyInsert(std::move(parsed), position);
@@ -508,7 +517,7 @@ void ChatFilters::received(const QVector<MTPDialogFilter> &list) {
 		applyRemove(position);
 		changed = true;
 	}
-	if (!ranges::contains(begin(_list), end(_list), 0, &ChatFilter::id)) {
+	if (!settings->hideAllChatsFolder && !ranges::contains(begin(_list), end(_list), 0, &ChatFilter::id)) {
 		_list.insert(begin(_list), ChatFilter());
 	}
 	if (changed || !_loaded || _reloading) {
@@ -519,9 +528,16 @@ void ChatFilters::received(const QVector<MTPDialogFilter> &list) {
 }
 
 void ChatFilters::apply(const MTPUpdate &update) {
+	// AyuGram hideAllChatsFolder
+	const auto settings = &AyuSettings::getInstance();
+
 	update.match([&](const MTPDupdateDialogFilter &data) {
 		if (const auto filter = data.vfilter()) {
-			set(ChatFilter::FromTL(*filter, _owner));
+			auto parsed = ChatFilter::FromTL(*filter, _owner);
+			if (settings->hideAllChatsFolder && parsed.id() == 0) {
+				return;
+			}
+			set(parsed);
 		} else {
 			remove(data.vid().v);
 		}
--- Telegram/SourceFiles/window/window_filters_menu.cpp
+++ Telegram/SourceFiles/window/window_filters_menu.cpp
@@ -40,6 +40,10 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "styles/style_layers.h" // attentionBoxButton
 #include "styles/style_menu_icons.h"
 
+// AyuGram includes
+#include "ayu/ayu_settings.h"
+
+
 namespace Window {
 
 FiltersMenu::FiltersMenu(
@@ -166,6 +170,9 @@ void FiltersMenu::scrollToButton(not_null<Ui::RpWidget*> widget) {
 }
 
 void FiltersMenu::refresh() {
+	// AyuGram hideAllChatsFolder
+	const auto settings = &AyuSettings::getInstance();
+
 	const auto filters = &_session->session().data().chatsFilters();
 	if (!filters->has() || _ignoreRefresh) {
 		return;
@@ -181,7 +188,7 @@ void FiltersMenu::refresh() {
 	const auto maxLimit = (reorderAll ? 1 : 0)
 		+ Data::PremiumLimits(&_session->session()).dialogFiltersCurrent();
 	const auto premiumFrom = (reorderAll ? 0 : 1) + maxLimit;
-	if (!reorderAll) {
+	if (!reorderAll && !settings->hideAllChatsFolder) {
 		_reorder->addPinnedInterval(0, 1);
 	}
 	_reorder->addPinnedInterval(
@@ -427,9 +434,12 @@ void FiltersMenu::applyReorder(
 		return;
 	}
 
+	// AyuGram hideAllChatsFolder
+	const auto settings = &AyuSettings::getInstance();
+
 	const auto filters = &_session->session().data().chatsFilters();
 	const auto &list = filters->list();
-	if (!premium()) {
+	if (!settings->hideAllChatsFolder && !premium()) {
 		if (list[0].id() != FilterId()) {
 			filters->moveAllToFront();
 		}
