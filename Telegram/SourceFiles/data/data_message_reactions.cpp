0f2e297036e49f97e6b6fca645550f433074dca6
ZavaruKitsu <alexeyzavar@gmail.com>
2023-09-30 15:04:54 +0300

    feat: implement read after actions

--- Telegram/SourceFiles/data/data_message_reactions.cpp
+++ Telegram/SourceFiles/data/data_message_reactions.cpp
@@ -39,6 +39,9 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "styles/style_chat.h"
 
 #include "base/random.h"
+// AyuGram includes
+#include "ayu/ayu_state.h"
+
 
 namespace Data {
 namespace {
@@ -1485,6 +1488,13 @@ void Reactions::send(not_null<HistoryItem*> item, bool addToRecent) {
 	)).done([=](const MTPUpdates &result) {
 		_sentRequests.remove(id);
 		_owner->session().api().applyUpdates(result);
+
+		const auto settings = &AyuSettings::getInstance();
+		if (!settings->sendReadMessages && settings->markReadAfterReaction && item)
+		{
+			AyuState::setAllowSendReadPacket(true);
+			item->history()->session().data().histories().readInboxOnNewMessage(item);
+		}
 	}).fail([=](const MTP::Error &error) {
 		_sentRequests.remove(id);
 	}).send();
