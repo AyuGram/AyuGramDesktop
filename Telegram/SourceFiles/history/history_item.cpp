188751ed10f4a6bb3874d43688df5564dcf47408
AlexeyZavar <sltkval1@gmail.com>
2024-03-13 17:25:32 +0300

    fix: mute exception

--- Telegram/SourceFiles/history/history_item.cpp
+++ Telegram/SourceFiles/history/history_item.cpp
@@ -3346,7 +3346,7 @@ void HistoryItem::setAyuHint(const QString &hint) {
 		history()->owner().requestItemViewRefresh(this);
 		history()->owner().requestItemResize(this);
 	} catch (...) {
-		LOG(("AyuGram: crash in setting hint"));
+		DEBUG_LOG(("AyuGram: crash in setting hint"));
 	}
 }
 
