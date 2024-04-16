698f481d1ace4274cac3a062a03e6b572bda7016
AlexeyZavar <sltkval1@gmail.com>
2024-04-17 02:38:43 +0300

    fix: make it build

--- Telegram/SourceFiles/chat_helpers/emoji_list_widget.h
+++ Telegram/SourceFiles/chat_helpers/emoji_list_widget.h
@@ -402,8 +402,6 @@ private:
 
 	void applyNextSearchQuery();
 
-	void showPreview();
-
 	const std::shared_ptr<Show> _show;
 	const ComposeFeatures _features;
 	const bool _onlyUnicodeEmoji;
@@ -484,9 +482,6 @@ private:
 	rpl::event_stream<EmojiChosen> _chosen;
 	rpl::event_stream<FileChosen> _customChosen;
 	rpl::event_stream<> _jumpedToPremium;
-
-	base::Timer _previewTimer;
-	bool _previewShown = false;
 
 };
 
