b1dbe58cd9621872b6ded401a2492b070f02533e
AlexeyZavar <sltkval1@gmail.com>
2024-05-25 04:33:05 +0300

    feat: round context menu a bit

--- Telegram/SourceFiles/inline_bots/inline_results_widget.cpp
+++ Telegram/SourceFiles/inline_bots/inline_results_widget.cpp
@@ -38,7 +38,7 @@ Widget::Widget(
 , _contentHeight(_contentMaxHeight)
 , _scroll(this, st::inlineBotsScroll)
 , _innerRounding(Ui::PrepareCornerPixmaps(
-	ImageRoundRadius::Small,
+	ImageRoundRadius::Large,
 	st::emojiPanBg))
 , _inlineRequestTimer([=] { onInlineRequest(); }) {
 	resize(QRect(0, 0, st::emojiPanWidth, _contentHeight).marginsAdded(innerPadding()).size());
@@ -66,7 +66,7 @@ Widget::Widget(
 	style::PaletteChanged(
 	) | rpl::start_with_next([=] {
 		_innerRounding = Ui::PrepareCornerPixmaps(
-			ImageRoundRadius::Small,
+			ImageRoundRadius::Large,
 			st::emojiPanBg);
 	}, lifetime());
 
@@ -242,7 +242,7 @@ void Widget::startShowAnimation() {
 			QRect(
 				inner.topLeft() * style::DevicePixelRatio(),
 				inner.size() * style::DevicePixelRatio()));
-		_showAnimation->setCornerMasks(Images::CornersMask(ImageRoundRadius::Small));
+		_showAnimation->setCornerMasks(Images::CornersMask(ImageRoundRadius::Large));
 		_showAnimation->start();
 	}
 	hideChildren();
