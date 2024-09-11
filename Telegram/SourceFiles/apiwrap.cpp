85bdd28b10df8be3a80a63d035c160ea61534c6a
AlexeyZavar <sltkval1@gmail.com>
2024-09-11 21:58:14 +0300

    fix: schedule tokenless bot start

--- Telegram/SourceFiles/apiwrap.cpp
+++ Telegram/SourceFiles/apiwrap.cpp
@@ -4282,6 +4282,13 @@ void ApiWrap::sendBotStart(
 		if (chat) {
 			message.textWithTags.text += '@' + bot->username();
 		}
+
+		const auto settings = &AyuSettings::getInstance();
+		if (settings->useScheduledMessages) {
+			auto current = base::unixtime::now();
+			message.action.options.scheduled = current + 12;
+		}
+
 		sendMessage(std::move(message));
 		return;
 	}
