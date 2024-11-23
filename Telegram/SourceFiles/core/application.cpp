15897ac7f3388389abddce8a0d1ce30ec0ded3ca
AlexeyZavar <sltkval1@gmail.com>
2024-11-23 12:34:26 +0300

    feat: wide messages

--- Telegram/SourceFiles/core/application.cpp
+++ Telegram/SourceFiles/core/application.cpp
@@ -97,6 +97,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include <ksandbox.h>
 // AyuGram includes
+#include "ayu/ayu_infra.h"
 #include "ayu/features/streamer_mode/streamer_mode.h"
 
 namespace Core {
@@ -288,6 +289,7 @@ void Application::run() {
 	_translator = std::make_unique<Lang::Translator>();
 	QCoreApplication::instance()->installTranslator(_translator.get());
 
+	AyuInfra::init();
 	style::StartManager(cScale());
 	Ui::InitTextOptions();
 	Ui::StartCachedCorners();
