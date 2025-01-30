104ec6403b8020026dfe1ec0520fb66c171546c3
AlexeyZavar <sltkval1@gmail.com>
2025-01-30 23:04:03 +0300

    feat: show badges in dialogs list

--- Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
+++ Telegram/SourceFiles/dialogs/dialogs_inner_widget.cpp
@@ -90,6 +90,10 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 #include <QtWidgets/QApplication>
 
+// AyuGram includes
+#include "styles/style_ayu_icons.h"
+
+
 namespace Dialogs {
 namespace {
 
@@ -1568,6 +1572,14 @@ void InnerWidget::paintPeerSearchResult(
 			: context.selected
 			? &st::dialogsVerifiedIconOver
 			: &st::dialogsVerifiedIcon),
+		.exteraOfficial = &ThreeStateIcon(
+			st::dialogsExteraOfficialIcon,
+			context.active,
+			context.selected),
+		.exteraSupporter = &ThreeStateIcon(
+			st::dialogsExteraSupporterIcon,
+			context.active,
+			context.selected),
 		.premium = &ThreeStateIcon(
 			st::dialogsPremiumIcon,
 			context.active,
--- Telegram/SourceFiles/history/view/history_view_top_bar_widget.cpp
+++ Telegram/SourceFiles/history/view/history_view_top_bar_widget.cpp
@@ -75,6 +75,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "data/data_chat_filters.h"
 #include "history/admin_log/history_admin_log_section.h"
 #include "styles/style_ayu_styles.h"
+#include "styles/style_ayu_icons.h"
 
 namespace HistoryView {
 namespace {
@@ -632,6 +633,8 @@ void TopBarWidget::paintTopBar(Painter &p) {
 			.nameWidth = _title.maxWidth(),
 			.outerWidth = width(),
 			.verified = &st::dialogsVerifiedIcon,
+			.exteraOfficial = &st::dialogsExteraOfficialIcon.icon,
+			.exteraSupporter = &st::dialogsExteraSupporterIcon.icon,
 			.premium = &st::dialogsPremiumIcon.icon,
 			.scam = &st::attentionButtonFg,
 			.direct = &st::windowSubTextFg,
