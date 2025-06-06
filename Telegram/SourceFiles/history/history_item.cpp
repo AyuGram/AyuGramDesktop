15ea3c24188d2702b1d808ae673a8fd509ddc170
AlexeyZavar <sltkval1@gmail.com>
2025-06-06 21:27:39 +0300

    fix: ttl messages destroying

--- Telegram/SourceFiles/history/history_item.cpp
+++ Telegram/SourceFiles/history/history_item.cpp
@@ -2329,6 +2329,12 @@ void HistoryItem::clearMediaAsExpired() {
 	if (!media || !media->ttlSeconds()) {
 		return;
 	}
+
+	const auto& settings = AyuSettings::getInstance();
+	if (settings.saveDeletedMessages) {
+		return;
+	}
+
 	if (const auto document = media->document()) {
 		applyEditionToHistoryCleared();
 		auto text = (document->isVideoFile()
