3050b9448f28dc5cf7e49859092726226e5a9a34
AlexeyZavar <sltkval1@gmail.com>
2025-07-15 19:48:19 +0300

    fix: disable video ads

--- Telegram/SourceFiles/data/components/sponsored_messages.cpp
+++ Telegram/SourceFiles/data/components/sponsored_messages.cpp
@@ -245,6 +245,11 @@ bool SponsoredMessages::canHaveFor(not_null<History*> history) const {
 }
 
 bool SponsoredMessages::canHaveFor(not_null<HistoryItem*> item) const {
+	const auto &settings = AyuSettings::getInstance();
+	if (settings.disableAds) {
+		return false;
+	}
+
 	return item->history()->peer->isBroadcast()
 		&& item->isRegular();
 }
@@ -430,6 +435,11 @@ void SponsoredMessages::parseForVideo(
 
 SponsoredForVideo SponsoredMessages::prepareForVideo(
 		not_null<PeerData*> peer) {
+	const auto &settings = AyuSettings::getInstance();
+	if (settings.disableAds) {
+		return {};
+	}
+
 	const auto i = _dataForVideo.find(peer);
 	if (i == end(_dataForVideo) || i->second.entries.empty()) {
 		return {};
