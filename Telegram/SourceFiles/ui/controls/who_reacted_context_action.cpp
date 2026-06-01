0e323973437307af5f0bb1389f2cb8eceda09662
Ivan <283494121+redactdeveloper@users.noreply.github.com>
2026-06-01 18:51:25 +0300

    Fix who-read submenu hover crash

--- Telegram/SourceFiles/ui/controls/who_reacted_context_action.cpp
+++ Telegram/SourceFiles/ui/controls/who_reacted_context_action.cpp
@@ -347,7 +347,7 @@ void Action::updateUserpicsFromContent() {
 }
 
 void Action::populateSubmenu() {
-	if (_content.participants.size() < 1) {
+	if (_content.participants.size() < 1 || !isEnabled()) {
 		_submenu.clear();
 		_parentMenu->removeSubmenu(action());
 		if (!isEnabled()) {
@@ -360,7 +360,15 @@ void Action::populateSubmenu() {
 		action(),
 		st::whoReadMenu);
 	_submenu.populate(submenu, _content);
-	_parentMenu->checkSubmenuShow();
+	if (isSelected() && lastTriggeredSource() == Menu::TriggeredSource::Mouse) {
+		PostponeCall(this, [=] {
+			if (isEnabled()
+				&& isSelected()
+				&& lastTriggeredSource() == Menu::TriggeredSource::Mouse) {
+				_parentMenu->checkSubmenuShow();
+			}
+		});
+	}
 }
 
 void Action::paint(Painter &p) {
