55b0499a7cfd63f9f7dd43d1a62efe2499cf10df
own-boldsbrain <fernando@yellosolarhub.com>
2026-07-14 19:38:58 -0300

    chore: clean PR scope and add explicit settings include

--- Telegram/SourceFiles/history/view/media/history_view_photo.cpp
+++ Telegram/SourceFiles/history/view/media/history_view_photo.cpp
@@ -46,6 +46,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "styles/style_chat_helpers.h"
 
 // AyuGram includes
+#include "ayu/ayu_settings.h"
 #include "ayu/features/message_shot/message_shot.h"
 #include "ayu/ui/ayu_userpic.h"
 
