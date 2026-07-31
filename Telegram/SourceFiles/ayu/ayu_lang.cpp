ae8606bb4f250f1519441d4e22e4b47829d72fc8
PH4N7OMx <averkostya@gmail.com>
2026-07-31 12:32:10 +0200

    ayu: load language translations from PH4N7OMx/Languages@main

--- Telegram/SourceFiles/ayu/ayu_lang.cpp
+++ Telegram/SourceFiles/ayu/ayu_lang.cpp
@@ -118,10 +118,10 @@ void AyuLanguage::fetchLanguage(const QString &id, const QString &baseId) {
 	// https://crowdin.com/project/ayugram/discussions/6
 	QUrl url;
 	if (!finalLangPackId.isEmpty() && !baseId.isEmpty() && !needFallback) {
-		url.setUrl(qsl("https://cdn.jsdelivr.net/gh/AyuGram/Languages@l10n_main/values/langs/%1/Shared.json").arg(
+		url.setUrl(qsl("https://cdn.jsdelivr.net/gh/PH4N7OMx/Languages@main/values/langs/%1/Shared.json").arg(
 			finalLangPackId));
 	} else {
-		url.setUrl(qsl("https://cdn.jsdelivr.net/gh/AyuGram/Languages@l10n_main/values/langs/%1/Shared.json").arg(
+		url.setUrl(qsl("https://cdn.jsdelivr.net/gh/PH4N7OMx/Languages@main/values/langs/%1/Shared.json").arg(
 			needFallback ? baseId : finalLangPackId));
 	}
 	_chkReply = networkManager.get(QNetworkRequest(url));
